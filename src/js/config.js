f4ab105e707135af3cc98ddccde94deced1f7f25
gabrielscar <gabi.scarlat16@gmail.com>
2021-09-21 19:02:12 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+// This file is for constants wich are used all over the softwear. They are written with uppercase cuz they are never changed
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '10ba43aa-869a-450a-b16f-859901bd6ef8';
+export const MODAL_CLOSE_SEC = 2.5;
